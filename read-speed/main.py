da6c38125c1cc33f3345e3d95055aefc72590a38
Brandon Gioggia <bgioggia@gmail.com>
2021-01-09 14:16:31 -0500

    Functions for handling passages to be read

--- read-speed/main.py
+++ read-speed/main.py
@@ -1,16 +1,11 @@
 import time
+import passages
 
 
-story = ('\nIt was the best of times, it was the worst of times, it was the age of wisdom, \n'
-          'it was the age of foolishness, it was the epoch of belief, it was the epoch of incredulity, \n'
-          'it was the season of Light, it was the season of Darkness, it was the spring of hope, \n'
-          'it was the winter of despair, we had everything before us, we had nothing before us, \n'
-          'we were all going direct to Heaven, we were all going direct the other way—in short, \n'
-          'the period was so far like the present period, that some of its noisiest authorities \n'
-          'insisted on its being received, for good or for evil, in the superlative degree of comparison only.\n')
-word_count = len(story.split(' '))
+#story = ()
+#word_count = len(story.split(' '))
 
-# void -> float
+# void -> wpm: float
 def getReadSpeed():
     print('Please click the button when you are ready to start reading.\n'
           'Click the button again when you have finished.')
@@ -22,6 +17,7 @@ def getReadSpeed():
     seconds_to_read = end_time - start_time
 
     # find words/sec
+    print(word_count)
     wps = word_count/seconds_to_read
 
     # converts to words/min
@@ -33,4 +29,8 @@ def getReadSpeed():
 
 
 if __name__ == '__main__':
-    print('your reading speed is ' + str(getReadSpeed()) + ' words per minute.')
+    psgs = passages.Passages()
+    test_string = psgs.get_passages(1)
+    print(test_string[0][1])
+
+    #print('your reading speed is ' + str(getReadSpeed()) + ' words per minute.')
